7478665250aa85e2eb2f33055ae5c51b6a62d8c2
jorgeglezca98 <alu0101038490@ull.edu.es>
2019-10-24 14:01:26 +0100

    Created project

---
+++ Proyecto final/Assets/asteroidsMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroidsMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rg = GetComponent<Rigidbody>();
+		System.Random random = new System.Random ();
+
+		rg.AddForce(new Vector3(random.Next(-10,11),random.Next(-10,11),random.Next(-10,11)), ForceMode.Impulse);
+		rg.AddTorque(new Vector3(random.Next(-200,201),random.Next(-200,201),random.Next(-200,201)), ForceMode.Impulse);
+		rg.transform.localScale = Vector3.one * random.Next(5,51);
+	}
+}
---
+++ Proyecto final/Assets/spaceshipMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceshipMovement : MonoBehaviour {
+
+	private Rigidbody rg;
+
+	// Use this for initialization
+	void Start () {
+		rg = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rg.AddForce(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime, ForceMode.Impulse);
+		
+		if(rg.rotation.z < -0.25){
+			rg.constraints = RigidbodyConstraints.FreezeRotationZ;
+		} else {
+			rg.AddTorque(Vector3.back * Input.GetAxis("Horizontal") * Time.deltaTime * 200);
+		}
+	}
+}
