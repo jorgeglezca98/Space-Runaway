b104b825021c7cca263f25c4f36afcd081162530
jorgeglezca98 <jorgeglezca98@gmail.com>
2019-10-27 10:47:16 +0000

    Destruction of bullets over time

---
+++ Proyecto final/Assets/bulletPerformance.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletPerformance : MonoBehaviour {
+
+	public int maxDistace = 100;
+	private Vector3 initialPosition;
+
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(initialPosition, transform.position) >= maxDistace){
+			destroyBullet();
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+    {
+		destroyBullet();
+    }
+
+	void destroyBullet(){
+		Destroy(gameObject);
+	}
+}
