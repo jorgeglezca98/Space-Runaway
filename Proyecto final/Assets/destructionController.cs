8e056d123b75eb178202df1554e0fcd62b172860
jorgeglezca98 <jorgeglezca98@gmail.com>
2019-11-06 10:40:48 +0000

    Life management (pending adjustment)

--- Proyecto final/Assets/destructionController.cs
+++ Proyecto final/Assets/destructionController.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class destructionController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public float life = 100f;
+	public int destructionDelay = 10;
 
-	void OnCollisionEnter(Collision collision)
-    {
-    	Vector3 center = GetComponent<Renderer>().bounds.center;
-    	float radius = GetComponent<Renderer>().bounds.size.z;
+	void OnCollisionEnter(Collision collision) {
 
-    	foreach (Transform child in transform)
-        {
-            child.transform.parent = null;
-            Rigidbody childrg = child.gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
-            childrg.useGravity = false;
-            childrg.AddExplosionForce(1000, center, radius);
-        }
+		life -= collision.relativeVelocity.magnitude + collision.rigidbody.mass;
+
+		if(life <= 0) {
+	    	Vector3 center = GetComponent<Renderer>().bounds.center;
+	    	float radius = GetComponent<Renderer>().bounds.size.z;
+
+	    	foreach (Transform child in transform) {
+	            child.transform.parent = null;
+	            Rigidbody childrg = child.gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
+	            childrg.useGravity = false;
+	            childrg.AddExplosionForce(1000, center, radius);
+	            Destroy(child.gameObject, destructionDelay);
+	        }
+
+	        Destroy(gameObject, destructionDelay);
+	    }
     }
 }
